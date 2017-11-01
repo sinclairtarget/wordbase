893085e2d1517423569e933ec043712d19fa55ee
Sinclair Target <sinclairtarget@gmail.com>
2017-10-31 23:31:42 -0400

    Working /entries route.

--- flask/db.py
+++ flask/db.py
@@ -2,32 +2,39 @@ import sqlite3
 from flask import g
 from wordbase_backend import app
 
-def get_db():
+def get():
     """Opens a new db connection if none exists in the app context."""
     if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
+        g.sqlite_db = connect()
     return g.sqlite_db
 
-def connect_db():
+def dict_factory(cursor, row):
+    """Converts query result rows to dicts."""
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def connect():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
+    rv.row_factory = dict_factory
     return rv
 
 @app.cli.command('initdb')
-def initdb_command():
+def init_command():
     """Initializes the database."""
-    init_db()
+    init()
     print('Initialized the database.')
 
-def init_db():
-    db = get_db()
+def init():
+    db = get()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
 
 @app.teardown_appcontext
-def close_db(error):
+def close(error):
     """Closes db at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
--- flask/wordbase_backend.py
+++ flask/wordbase_backend.py
@@ -11,3 +11,10 @@ app.config.update({
 @app.route('/ping')
 def pong():
     return 'PONG'
+
+@app.route('/entries')
+def entries():
+    entries = db.get() \
+                .execute('select * from entries') \
+                .fetchall()
+    return jsonify(entries)
